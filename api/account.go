3004531c12dd45e36305979d99d7b28b4e2a12f9
Andrew Monteiro <andrew.agoravai@gmail.com>
2023-09-10 22:33:18 -0300

    feat: add route to update an account by its id

--- api/account.go
+++ api/account.go
@@ -96,6 +96,46 @@ func (server *Server) getAccount(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, foundAccount)
 }
 
+type updateAccountRequest struct {
+	params struct {
+		ID int64 `uri:"id" binding:"required,min=1"`
+	}
+	body struct {
+		Balance int64 `json:"balance" binding:"required,min=1"`
+	}
+}
+
+func (server *Server) updateAccount(ctx *gin.Context) {
+	var req updateAccountRequest
+
+	if err := ctx.ShouldBindUri(&req.params); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	if err := ctx.ShouldBindJSON(&req.body); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	updatedAccount, err := server.store.UpdateAccount(ctx, db.UpdateAccountParams{
+		ID:      req.params.ID,
+		Balance: req.body.Balance,
+	})
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, updatedAccount)
+}
+
 type deleteAccountRequest struct {
 	ID int64 `uri:"id" binding:"required,min=1"`
 }
