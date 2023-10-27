13a21b757b31c30f5945ebc9fa240e080f16ab7d
Andrew Monteiro <andrew.agoravai@gmail.com>
2023-10-27 00:36:55 -0300

    test: add missing integration tests to createTransfer method of Transfer

--- api/transfer_test.go
+++ api/transfer_test.go
@@ -2,12 +2,12 @@ package api
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
 
 	mockdb "github.com/Andrew-2609/simple-bank/db/mock"
 	db "github.com/Andrew-2609/simple-bank/db/sqlc"
@@ -30,6 +30,12 @@ func createRandomAccounts() [2]db.Account {
 			Currency: util.BRL,
 		},
 	}
+}
+
+func unmarshallTransfer(t *testing.T, responseBody *bytes.Buffer) db.TransferTxResult {
+	responseTransfer, err := util.UnmarshallJsonBody[db.TransferTxResult](responseBody)
+	require.NoError(t, err)
+	return responseTransfer
 }
 
 func TestCreateTransferAPI(t *testing.T) {
@@ -55,7 +61,6 @@ func TestCreateTransferAPI(t *testing.T) {
 			FromAccountID: accounts[0].ID,
 			ToAccountID:   accounts[1].ID,
 			Amount:        amount,
-			CreatedAt:     time.Now(),
 		},
 		FromAccount: accounts[0],
 		ToAccount:   accounts[1],
@@ -63,13 +68,11 @@ func TestCreateTransferAPI(t *testing.T) {
 			ID:        1,
 			AccountID: accounts[0].ID,
 			Amount:    -amount,
-			CreatedAt: time.Now(),
 		},
 		ToEntry: db.Entry{
 			ID:        2,
 			AccountID: accounts[1].ID,
 			Amount:    amount,
-			CreatedAt: time.Now(),
 		},
 	}
 
@@ -98,6 +101,149 @@ func TestCreateTransferAPI(t *testing.T) {
 			},
 			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
 				require.Equal(t, http.StatusCreated, recorder.Code)
+				require.Equal(t, expectedResult, unmarshallTransfer(t, recorder.Body))
+			},
+		},
+		{
+			name: "Bad Request",
+			arg: CreateTransferRequest{
+				FromAccountID: accounts[0].ID,
+				ToAccountID:   accounts[1].ID,
+				Amount:        amount,
+				Currency:      "AUD",
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Any()).Times(0)
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Any()).Times(0)
+				store.EXPECT().TransferTx(gomock.Any(), gomock.Any()).Times(0)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+				require.Exactly(t, map[string]interface{}{"error": "Key: 'CreateTransferRequest.Currency' Error:Field validation for 'Currency' failed on the 'currency' tag"}, UnmarshallAny(t, recorder.Body))
+			},
+		},
+		{
+			name: "Not Found - FromAccount",
+			arg: CreateTransferRequest{
+				FromAccountID: accounts[0].ID + 1,
+				ToAccountID:   accounts[0].ID,
+				Amount:        amount,
+				Currency:      "BRL",
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(accounts[0].ID+1)).
+					Times(1).
+					Return(db.Account{}, sql.ErrNoRows)
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Any()).Times(0)
+				store.EXPECT().TransferTx(gomock.Any(), gomock.Any()).Times(0)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+				require.Exactly(t, map[string]interface{}{"error": sql.ErrNoRows.Error()}, UnmarshallAny(t, recorder.Body))
+			},
+		},
+		{
+			name: "Not Found - ToAccount",
+			arg: CreateTransferRequest{
+				FromAccountID: accounts[0].ID,
+				ToAccountID:   accounts[1].ID + 1,
+				Amount:        amount,
+				Currency:      "BRL",
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(accounts[0].ID)).
+					Times(1).
+					Return(accounts[0], nil)
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(accounts[1].ID+1)).
+					Times(1).
+					Return(db.Account{}, sql.ErrNoRows)
+				store.EXPECT().TransferTx(gomock.Any(), gomock.Any()).Times(0)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+				require.Exactly(t, map[string]interface{}{"error": sql.ErrNoRows.Error()}, UnmarshallAny(t, recorder.Body))
+			},
+		},
+		{
+			name: "Bad Request - Different Currency",
+			arg: CreateTransferRequest{
+				FromAccountID: accounts[0].ID,
+				ToAccountID:   accounts[1].ID,
+				Amount:        amount,
+				Currency:      "USD",
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(accounts[0].ID)).
+					Times(1).
+					Return(accounts[0], nil)
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(accounts[1].ID)).Times(0)
+				store.EXPECT().TransferTx(gomock.Any(), gomock.Any()).Times(0)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusUnprocessableEntity, recorder.Code)
+			},
+		},
+		{
+			name: "Internal Server Error - Validate From Account Transfer",
+			arg:  validArg,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(accounts[0].ID)).
+					Times(1).
+					Return(db.Account{}, sql.ErrConnDone)
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Any()).Times(0)
+				store.EXPECT().TransferTx(gomock.Any(), gomock.Any()).Times(0)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+				require.Exactly(t, map[string]interface{}{"error": sql.ErrConnDone.Error()}, unmarshallAny(t, recorder.Body))
+			},
+		},
+		{
+			name: "Internal Server Error - Validate To Account Transfer",
+			arg:  validArg,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(accounts[0].ID)).
+					Times(1).
+					Return(accounts[0], nil)
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(accounts[1].ID)).
+					Times(1).
+					Return(db.Account{}, sql.ErrConnDone)
+				store.EXPECT().TransferTx(gomock.Any(), gomock.Any()).Times(0)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+				require.Exactly(t, map[string]interface{}{"error": sql.ErrConnDone.Error()}, unmarshallAny(t, recorder.Body))
+			},
+		},
+		{
+			name: "Internal Server Error",
+			arg:  validArg,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(accounts[0].ID)).
+					Times(1).
+					Return(accounts[0], nil)
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(accounts[1].ID)).
+					Times(1).
+					Return(accounts[1], nil)
+				store.EXPECT().
+					TransferTx(gomock.Any(), gomock.Eq(expectedArg)).
+					Times(1).
+					Return(db.TransferTxResult{}, sql.ErrTxDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+				require.Exactly(t, map[string]interface{}{"error": sql.ErrTxDone.Error()}, unmarshallAny(t, recorder.Body))
 			},
 		},
 	}
