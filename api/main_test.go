51a90925ad02a1a1d43dc1cc90e9f5c5e5954b58
Andrew Monteiro <andrew.agoravai@gmail.com>
2023-10-27 00:36:31 -0300

    test: implement helper function UnmarshallAny at main_test.go

--- api/main_test.go
+++ api/main_test.go
@@ -1,13 +1,16 @@
 package api
 
 import (
+	"bytes"
 	"database/sql"
 	"os"
 	"testing"
 
 	db "github.com/Andrew-2609/simple-bank/db/sqlc"
+	"github.com/Andrew-2609/simple-bank/util"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/require"
 )
 
 var (
@@ -18,4 +21,10 @@ var (
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
 	os.Exit(m.Run())
+}
+
+func UnmarshallAny(t *testing.T, responseBody *bytes.Buffer) any {
+	unmarshalledObject, err := util.UnmarshallJsonBody[any](responseBody)
+	require.NoError(t, err)
+	return unmarshalledObject
 }
