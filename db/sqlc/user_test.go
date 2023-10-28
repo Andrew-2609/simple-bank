9b2b8e9e8f5d183184df1efe05d4c6842372f76e
Andrew Monteiro <andrew.agoravai@gmail.com>
2023-10-27 21:19:52 -0300

    test: add integration tests for users table

---
+++ db/sqlc/user_test.go
@@ -0,0 +1,49 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Andrew-2609/simple-bank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomUser(t *testing.T) (user User) {
+	arg := CreateUserParams{
+		Username: util.RandomOwner(),
+		Password: "secret",
+		Name:     util.RandomOwner(),
+		LastName: util.RandomOwner(),
+		Email:    util.RandomEmail(),
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, arg.Username, user.Username)
+	require.Equal(t, arg.Password, user.Password)
+	require.Equal(t, arg.Name, user.Name)
+	require.Equal(t, arg.LastName, user.LastName)
+	require.Equal(t, arg.Email, user.Email)
+	require.True(t, user.PasswordChangedAt.IsZero())
+	require.NotZero(t, user.CreatedAt)
+
+	return
+}
+
+func TestCreateUser(t *testing.T) {
+	createRandomUser(t)
+}
+
+func TestGetUser(t *testing.T) {
+	user := createRandomUser(t)
+
+	foundUser, err := testQueries.GetUser(context.Background(), user.Username)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, foundUser)
+
+	require.Exactly(t, user, foundUser)
+}
