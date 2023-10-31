ac0748182d635eb4599db8c09fbfa01023942cd0
Andrew Monteiro <andrew.agoravai@gmail.com>
2023-10-30 22:49:35 -0300

    fix: add users email UNIQUE constraint

--- db/migrations/000002_add_users_table.up.sql
+++ db/migrations/000002_add_users_table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE "users" (
   "password" varchar NOT NULL,
   "name" varchar NOT NULL,
   "last_name" varchar NOT NULL,
-  "email" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
   "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
